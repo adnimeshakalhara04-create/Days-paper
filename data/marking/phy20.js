0a3356291dce6db74677b223293039685bed964d
A.D Nimesha Kalhara <a.dnimeshakalhara04@gmail.com>
2026-08-13 15:18:03 +0530

    Fix PHY 20 browser imports

--- data/marking/phy20.js
+++ data/marking/phy20.js
@@ -1,8 +1,7 @@
-import q01 from './phy20/q01';
-import q02 from './phy20/q02';
-import q03 from './phy20/q03';
-import q04 from './phy20/q04';
-import q05 from './phy20/q05';
-import q06 from './phy20/q06';
-
-export default [q01, q02, q03, q04, q05, q06];
+import q01 from './phy20/q01.js';
+import q02 from './phy20/q02.js';
+import q03 from './phy20/q03.js';
+import q04 from './phy20/q04.js';
+import q05 from './phy20/q05.js';
+import q06 from './phy20/q06.js';
+export default [q01,q02,q03,q04,q05,q06];
