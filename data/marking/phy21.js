8e5e479dbbae0d7adbb8ba0f619d0ffe923a8fc0
A.D Nimesha Kalhara <a.dnimeshakalhara04@gmail.com>
2026-08-13 15:18:13 +0530

    Fix PHY 21 browser imports

--- data/marking/phy21.js
+++ data/marking/phy21.js
@@ -1,6 +1,5 @@
-import q01 from './phy21/q01';
-import q02 from './phy21/q02';
-import q03 from './phy21/q03';
-import q04 from './phy21/q04';
-
-export default [q01, q02, q03, q04];
+import q01 from './phy21/q01.js';
+import q02 from './phy21/q02.js';
+import q03 from './phy21/q03.js';
+import q04 from './phy21/q04.js';
+export default [q01,q02,q03,q04];
