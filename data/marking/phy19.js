35a404c7feae4e2302ba2895a16ce2899400df8b
A.D Nimesha Kalhara <a.dnimeshakalhara04@gmail.com>
2026-08-13 15:17:55 +0530

    Fix PHY 19 browser imports

--- data/marking/phy19.js
+++ data/marking/phy19.js
@@ -1,6 +1,5 @@
-import q01 from './phy19/q01';
-import q02 from './phy19/q02';
-import q03 from './phy19/q03';
-import q04 from './phy19/q04';
-
-export default [q01, q02, q03, q04];
+import q01 from './phy19/q01.js';
+import q02 from './phy19/q02.js';
+import q03 from './phy19/q03.js';
+import q04 from './phy19/q04.js';
+export default [q01,q02,q03,q04];
