d98b20ee60abee85e27ba25e96639fcae1fd66a7
A.D Nimesha Kalhara <a.dnimeshakalhara04@gmail.com>
2026-08-13 14:25:08 +0530

    Assemble PHY 20 marking module

---
+++ data/marking/phy20.js
@@ -0,0 +1,8 @@
+import q01 from './phy20/q01';
+import q02 from './phy20/q02';
+import q03 from './phy20/q03';
+import q04 from './phy20/q04';
+import q05 from './phy20/q05';
+import q06 from './phy20/q06';
+
+export default [q01, q02, q03, q04, q05, q06];
